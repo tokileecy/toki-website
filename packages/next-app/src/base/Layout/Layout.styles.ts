fe1ae2084299b582b9d191b34456fff62fb94bca
tokileecy <tokileecy@gmail.com>
2022-01-24 07:25:23 +0000

    feat: adjust Layout styles

---
+++ packages/next-app/src/base/Layout/Layout.styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/css'
+
+export const webglLayer = css`
+  top: 13%;
+  border: solid 2px rgba(255, 255, 255, 0.8);
+  border-left: 0;
+  border-right: 0;
+  height: 74%;
+`
--- packages/next-app/src/base/Layout/Layout.tsx
+++ packages/next-app/src/base/Layout/Layout.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import BaseLayout, {
   BaseLayoutProps,
 } from '@psycholog-studio/ui/Layouts/BaseLayout'
-import { threeManager } from './core'
+import { threeManager } from '../core'
+import * as styles from './Layout.styles'
 
 export type LayoutProps = Omit<BaseLayoutProps, 'threeManagerRef'>
 
 const Layout = (props: BaseLayoutProps): JSX.Element => {
-  return <BaseLayout {...props} threeManager={threeManager} />
+  return (
+    <BaseLayout
+      {...props}
+      threeManager={threeManager}
+      classes={{
+        webglLayer: styles.webglLayer,
+      }}
+    />
+  )
 }
 
 export default Layout
